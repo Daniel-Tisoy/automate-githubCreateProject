3a42456e304edeb2fd4a9c2e1425f692042d88e5
luis-dt <danieltisoy2001@gmail.com>
2020-12-22 09:01:18 -0500

    create.py v.1

--- create.py
+++ create.py
@@ -12,12 +12,14 @@ load_dotenv()
 path = os.getenv("FILEPATH")
 github_token = os.getenv('GITHUB_TOKEN')
 
+
 def create():
     folderName = str(sys.argv[1])
     os.makedirs(path + str(folderName))
     user = Github(github_token).get_user()
     user.create_repo(folderName)
     logger.info("Succesfully created repository {}".format(folderName))
+
 
 if __name__ == "__main__":
     create()
